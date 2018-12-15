f0021d62abcf1a586fc42e8566f95226c10e6a65
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-12-15 22:34:04 +0200

    Add call to Shopify API and basic view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,36 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+
     @webhooks = ShopifyAPI::Webhook.find(:all)
+
+    @date = DateTime.now.to_date
+
+    if params[:date]
+      @date = params[:date].to_date
+    end
+
+    @orders = ShopifyAPI::Order.find(:all,params: {
+      processed_at_min: @date,
+      processed_at_max: @date+1,
+      status: "any",
+      limit: 50,
+      })
+
+    @topItems = calculate_top_3_items_from(@orders)
   end
+
+  private
+  def calculate_top_3_items_from(orders)
+    summary = Hash.new(0)
+
+    orders.each do |order|
+      order.line_items.each do |item|
+        summary[item.title] += item.quantity
+      end
+    end
+
+    summary.sort_by { |_, v| -v }.first(3)
+  end
+
+
 end
